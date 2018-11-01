3845ed244119ed1f943ae8c7c38ec284782bae89
Gordon Balaaka <40589388+balaakagordon@users.noreply.github.com>
2018-11-01 19:34:01 +0300

    [Chore #160583955] Add routing to the app, for navigation. (#4)
    - Merge with develop and revert App component back to a class component
    
    [Chore #160583957] add code climate coverage test after passing the tests from circleci (#6)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import './App.css';
+import LoginPage from './views/login';
+import HomePage from './views/home';
+import ErrorNotFound from './views/notfound';
 
 class App extends Component {
   render() {
     return (
-      <h1>  Welcome to Authors-Haven</h1>
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route component={ErrorNotFound} />
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
---
+++ src/views/notfound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const ErrorNotFound = () => (
+  <h1>404: This page cannot be found!</h1>
+);
+
+
+export default ErrorNotFound;
