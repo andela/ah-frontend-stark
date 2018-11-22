6806188531fb86e5ed87ce6ff2d5df948d42d25f
Edward Kigozi <eddyk1collab@gmail.com>
2018-11-23 01:12:38 +0300

    [Bug] change default url

--- src/common/base.js
+++ src/common/base.js
@@ -1,2 +1,2 @@
 export const SOCIAL_REDIRECT_URL = `${window.location.origin}/social/verify/`;
-export const SOCIAL_LAUNCH_DEFAULT_URL = 'https://ah-backend-stark-staging.herokuapp.com/api/auth/google/';
+export const SOCIAL_LAUNCH_DEFAULT_URL = 'https://ah-backend-stark-staging.herokuapp.com/api/auth/';
